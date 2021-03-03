226dabef71fa30d52543d79c9e7726db12bf4c71
Sarwar Maruf <55227455+Sarwar-Maruf@users.noreply.github.com>
2021-03-03 13:49:56 +0600

    first commit

---
+++ textutils/views.py
@@ -0,0 +1,58 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def analyze(request):
+    djtext = request.POST.get('text', 'default')
+    removepunc = request.POST.get('removepunc', 'off')
+    uppercase = request.POST.get('uppercase', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    extraspace = request.POST.get('extraspace', 'off')
+
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+
+        params = {'purpose':'After Analyzing', 'analyzed_text': analyzed}
+        djtext = analyzed
+        
+    if(uppercase=="on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+
+        params = {'purpose': 'After Analyzing', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if(extraspace=="on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'After Analyzing', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if (newlineremover == "on"):
+        analyzed = ""
+        for char in djtext:
+            if char != "\n" and char!="\r":
+                analyzed = analyzed + char
+            else:
+                pass
+        params = {'purpose': 'After Analyzing', 'analyzed_text': analyzed}
+
+    if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on"):
+        return HttpResponse("please select any operation and try again")
+
+    return render(request, 'analyzed.html', params)
+
+
+
+
