4bdd48a75a1bc78dd3b90bd7f293dbac6bc2cb03
Sarwar-Maruf <sarwar.maruf9@gmail.com>
2021-05-01 22:14:54 +0600

    text analyzer

--- textutils/settings.py
+++ textutils/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_62+qur(*$np*wks#e*8sca&mrby%6dy0ic3u=o65ygmu)q&yd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
